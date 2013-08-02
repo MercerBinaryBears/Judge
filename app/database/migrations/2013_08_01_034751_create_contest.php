3bc4c17172ab8a7f392d60903753442a00e7bddc
Chip Bell <chipbell4@gmail.com>
2013-08-01 20:47:50 -0400

    Added an ending time for the contest

--- app/database/migrations/2013_08_01_034751_create_contest.php
+++ app/database/migrations/2013_08_01_034751_create_contest.php
@@ -15,6 +15,7 @@ class CreateContest extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->timestamp('starts_at');
+			$table->timestamp('ends_at');
 			$table->timestamps();
 		});
 	}
