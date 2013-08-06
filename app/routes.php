b9dfcd71b53137afb10b13a6cfc7fa149b49c57d
Chip Bell <chipbell4@gmail.com>
2013-08-05 20:15:47 -0400

    Named routes in routes file

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,18 @@
 <?php
 
 // home routes
-Route::get('/', 'HomeController@index');
-Route::get('/scoreboard', 'HomeController@scoreboard');
+Route::get('/', array('as'=>'index', 'uses'=>'HomeController@index'));
+Route::get('/scoreboard', array('as'=>'scoreboard', 'uses'=>'HomeController@scoreboard'));
 
 // judge routes
 Route::group(array('before'=>'judge'), function(){
-	Route::get('/judge', 'SolutionController@judgeIndex');
-	Route::get('/judge/solutions/{id}/edit', 'SolutionController@edit');
-	Route::post('/judge/solutions/{id}/edit', 'SolutionController@update');
+	Route::get('/judge',  array('as'=>'judge_index', 'uses'=>'SolutionController@judgeIndex'));
+	Route::get('/judge/solutions/{id}/edit', array('as'=>'edit_solution', 'uses'=>'SolutionController@edit'));
+	Route::post('/judge/solutions/{id}/edit', array('as'=>'update_solution', 'uses'=>'SolutionController@update'));
 });
 
 /*
 	User routes
 */
-Route::post('/login', 'UserController@login');
-Route::get('/logout', 'UserController@logout');
+Route::post('/login', array('as'=>'login', 'uses'=>'UserController@login'));
+Route::get('/logout', array('as'=>'logout', 'uses'=>'UserController@logout'));
