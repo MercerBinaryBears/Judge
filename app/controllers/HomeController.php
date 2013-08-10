0e268d3795b0d452d556c4f29eaa14081d0afa74
Chip Bell <chipbell4@gmail.com>
2013-08-09 23:01:02 -0400

    Commented the controllers a good bit more

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,15 +2,11 @@
 
 class HomeController extends BaseController {
 
+	/**
+	 * The index route for the Judge site. Will contain a scoreboard
+	 * that auto-refreshes (we can do that part later)
+	 */
 	public function index() {
 		return View::make('index');
-	}
-
-	public function portal() {
-		return View::make('portal');
-	}
-
-	public function scoreboard() {
-		return View::make('scoreboard');
 	}
 }
