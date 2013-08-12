a2a58aed614cdb8aa147ca23fb1b8321c7a4f24a
Chip Bell <chipbell4@gmail.com>
2013-08-11 21:17:23 -0400

    Improved autoloading, fleshed out algorithm more

--- app/library/scripts/lib/SolutionJudger.py
+++ app/library/scripts/lib/SolutionJudger.py
@@ -1,4 +1,5 @@
 import commands
+import importlib
 
 class SolutionJudger(object):
 	'''Judges a passed solution by autodetecting the language and loading the correct language judging class'''
@@ -29,12 +30,13 @@ class SolutionJudger(object):
 
 		# now, attempt to auto import it
 		# (via http://stackoverflow.com/questions/4821104)
-		module_name = 'libs.languages.' + languageJudgeClassName
-		if self.debug:
-			print 'Attempting to autoload',module_name
+		languageJudgeModule = importlib.import_module("lib.languages.{0}".format(languageJudgeClassName))
 
-		module = __import__(module_name)
-		return getattr(module, languageJudgeClassName)
+		if self.debug:
+			print 'Loaded module: ',
+			print languageJudgeModule
+
+		return getattr(languageJudgeModule, languageJudgeClassName)
 
 	def compile(self):
 		'''Attempts to compile the passed file. Returns a tuple with the compile result, and output'''
@@ -45,32 +47,38 @@ class SolutionJudger(object):
 		# get the status and output
 		status, output = commands.getstatusoutput(self.judger.getCompileString())
 
-		if debug:
+		if self.debug:
 			print 'Status {0}\nOutput:\n{1}'.format(status, output)
 
 
 		# check the result with the class. TODO: just use the status code instead!
-		did_succeed = self.judge.checkCompilePassed(output)
+		did_succeed = self.judger.checkCompilePassed(output)
 
 		return (did_succeed, output)
 
 	def run(self, input_file='/dev/null', output_file='/dev/null'):
 		'''Runs the compiled output from the passed file on the passed input file, redirected in the output file, returning the status code'''
 
-		run_command += "{0} < {1} > {2}".format(self.judger.getRunCommand(), input_file, output_file)
+		run_command = "{0} < {1} > {2}".format(self.judger.getRunCommand(), input_file, output_file)
 
-		if debug:
+		if self.debug:
 			print "Command to run is " + run_command
+
+		# assign this output file to variable, so when they run a diff, we don't have to pass it
+		self.program_output_file = output_file
 
 		status, output = commands.getstatusoutput(run_command)
 
 		return status, output
 
-	def diff(self, program_output_file='/dev/null', expected_output_file='/dev/null'):
+	def diff(self, expected_output_file='/dev/null', program_output_file=None):
 		'''Checks the output of a program with the correct output, returns a tuple with a boolean match, and the diff output'''
 
-		if debug:
-			print "Comparing program output {0} and judging output {1}".format(program_output_file, expected_output_file)
+		if program_output_file != None:
+			self.program_output_file = program_output_file
+
+		if self.debug:
+			print "Comparing program output {0} and judging output {1}".format(self.program_output_file, expected_output_file)
 
 		status, output = commands.getstatusoutput('diff {0} {1}')
 
