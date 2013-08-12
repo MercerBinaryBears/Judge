50ffda743cfba10f878c265b5d808a69be56530c
Chip Bell <chipbell4@gmail.com>
2013-08-11 21:17:57 -0400

    Added Python Judger

---
+++ app/library/scripts/lib/languages/PyJudge.py
@@ -0,0 +1,11 @@
+from LanguageJudge import LanguageJudge
+
+class PyJudge(LanguageJudge):
+	def getCompileString(self):
+		return 'echo \'\''
+
+	def checkCompilePassed(self, output):
+		return True
+
+	def getRunCommand(self):
+		return "python {0}".format(self.filename)
