84bda25b986c895f7f8ffedbda8f8c4e46c472dd
Chip Bell <chipbell4@gmail.com>
2013-08-17 14:35:35 -0400

    Styled solution edit form

--- app/views/forms/edit_solution.blade.php
+++ app/views/forms/edit_solution.blade.php
@@ -1,21 +1,32 @@
 @extends('layout')
 
 @section('content')
-Solution for {{ $solution->problem->name }} <br/>
-Submitted by {{ $solution->user->username }} at {{ $solution->created_at }} <br/>
 
-Submitted in {{$solution->language->name}} <br/>
+<div class="span6">
+	<dl class='dl-horizontal'>
+		<dt>Problem</dt>
+		<dd>{{ $solution->problem->name }}</dt>
+		<dt>Submitted</dt>
+		<dd>{{ $solution->created_at }}</dd>
+		<dt>Team</dt>
+		<dd>{{$solution->user->username}}</dd>
+		<dt>Language</dt>
+		<dd>{{$solution->language->name}}</dd>
+		<dt>Download</dt>
+		<dd>{{ link_to_route('solution_package', 'Solution Package', array($solution->id), array('_target'=>'blank'))}}</dd>
+	</dl>
+</div>
 
-Download {{ link_to_route('solution_package', 'Solution Package', array($solution->id), array('_target'=>'blank'))}}
+<div class="span6">
+	{{ Form::open(array('route'=>array("edit_solution", $solution->id))) }}
+	{{ Form::label('solution_state_id', 'Solution State: ') }}
+	{{ Form::select('solution_state_id', $solution_states)}}
+	{{ Form::submit('Save')}}
+	{{ Form::close() }}
 
-{{ Form::open(array('route'=>array("edit_solution", $solution->id))) }}
-{{ Form::label('solution_state_id', 'Solution State: ') }}
-{{ Form::select('solution_state_id', $solution_states)}}
-{{ Form::submit('Save')}}
-{{ Form::close() }}
-
-{{ Form::open(array('route'=>array("unclaim_solution", $solution->id)))}}
-{{ Form::submit('Unclaim') }}
-{{ Form::close() }}
+	{{ Form::open(array('route'=>array("unclaim_solution", $solution->id)))}}
+	{{ Form::submit('Unclaim') }}
+	{{ Form::close() }}
+</div>
 
 @stop
