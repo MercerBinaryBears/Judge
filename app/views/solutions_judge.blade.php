0afe29031c5e13f04ebf7c595c52a55647306333
Chip Bell <chipbell4@gmail.com>
2013-08-17 14:31:39 -0400

    Judging solution listing styling

--- app/views/solutions_judge.blade.php
+++ app/views/solutions_judge.blade.php
@@ -1,23 +1,44 @@
 @extends('layout')
 
 @section('content')
-	<h3>Unjudged Solutions</h3>
-	<ul class="solutions">
-	@foreach($unjudged_solutions as $solution)
-		<li>
-			Solution {{$solution->created_at}} for {{$solution->problem->name}} by {{$solution->user->username}} <br/>
-			{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array()) }}
-		</li>
-	@endforeach
-	</ul>
-	<h3>Your Judged Solutions</h3>
-	<ul class="solutions">
-	@foreach($claimed_solutions as $solution)
-		<li>
-			Solution {{$solution->created_at}} for {{$solution->problem->name}} by {{$solution->user->username}} <br/>
-			You judged as {{ $solution->solution_state->name }} <br/>
-			{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array()) }}
-		</li>
-	@endforeach
-	</ul>
+	<div class="span6">
+		<h3>Unjudged Solutions</h3>
+		<ul class="solutions unstyled">
+		@foreach($unjudged_solutions as $solution)
+			<li>
+				<dl class='dl-horizontal'>
+					<dt>Problem</dt>
+					<dd>{{ $solution->problem->name }}</dt>
+					<dt>Submitted</dt>
+					<dd>{{ $solution->created_at }}</dd>
+					<dt>Team</dt>
+					<dd>{{$solution->user->username}}</dd>
+					<dt></dt>
+					<dd>{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array()) }}</dd>
+				</dl>
+			</li>
+		@endforeach
+		</ul>
+	</div>
+	<div class="span6">
+		<h3>Your Judged Solutions</h3>
+		<ul class="solutions unstyled">
+		@foreach($claimed_solutions as $solution)
+			<li>
+				<dl class='dl-horizontal'>
+					<dt>Problem</dt>
+					<dd>{{ $solution->problem->name }}</dt>
+					<dt>Submitted</dt>
+					<dd>{{ $solution->created_at }}</dd>
+					<dt>Team</dt>
+					<dd>{{ $solution->user->username}}</dd>
+					<dt>Judged as</dt>
+					<dd>{{ $solution->solution_state->name }}</dd>
+					<dt></dt>
+					<dd>{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array()) }}</dd>
+				</dl>
+			</li>
+		@endforeach
+		</ul>
+	</div>
 @stop
