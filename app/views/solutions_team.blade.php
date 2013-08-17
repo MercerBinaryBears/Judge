94dae54af864482721584c375a2be40d3ee6ed10
Chip Bell <chipbell4@gmail.com>
2013-08-17 14:23:22 -0400

    Team stylings

--- app/views/solutions_team.blade.php
+++ app/views/solutions_team.blade.php
@@ -1,18 +1,30 @@
 @extends('layout')
 
 @section('content')
-<ul class="solutions">
-	@foreach($solutions as $solution)
-	<li>
-		Solution submitted at {{ $solution->created_at }}
-		for {{ $solution->problem->name }} <br/>
-		State: {{ $solution->solutionState->name}}
-		@if($solution->claiming_judge_id != null)
-			Judged by {{$solution->claimingJudge->username}}
-		@endif
-	</li>
-	@endforeach
-</ul>
+<div class="span3">
+	@include('forms.create_solution')
+</div>
+<div class="span9">
+	<ul class="solutions unstyled">
+		@foreach($solutions as $solution)
+		<li>
+			<dl class='dl-horizontal'>
+				<dt>Problem</dt>
+				<dd>{{ $solution->problem->name }}</dt>
+				<dt>Submitted</dt>
+				<dd>{{ $solution->created_at }}</dd>
+				<dt>State</dt>
+				<dd>{{ $solution->solutionState->name}}</dd>
+				<dt>Judged By</dt>
+				<dd>
+					@if($solution->claiming_judge_id != null)
+						Judged by {{$solution->claimingJudge->username}}
+					@endif
+				</dd>
+			</dl>
+		</li>
+		@endforeach
+	</ul>
+</div>
 
-@include('forms.create_solution')
 @stop
