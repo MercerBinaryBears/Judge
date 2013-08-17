6466e9625d4c03fd30bf97e2602f2c91f6de2eac
Chip Bell <chipbell4@gmail.com>
2013-08-16 21:16:08 -0400

    Added bootstrap to project

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,8 +3,10 @@
 	<head>
 		<title>Judge</title>
 		@section('scripts')
+			<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
 		@show
 		@section('styles')
+			<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
 		@show
 	</head>
 	<body>
