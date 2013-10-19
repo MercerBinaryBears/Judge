63b06228844971313e43497ab8d70507b81f5871
Chip Bell <chipbell4@gmail.com>
2013-10-18 21:09:23 -0400

    Resolved #46

--- app/controllers/TeamController.php
+++ app/controllers/TeamController.php
@@ -12,7 +12,7 @@ class TeamController extends BaseController {
 
 		return View::make('solutions_team')
 			->with('solutions', Solution::forCurrentContest()->where('user_id', Auth::user()->id)->get())
-			->with('problems', Problem::lists('name', 'id'))
+			->with('problems', Problem::forCurrentContest()->lists('name', 'id'))
 			->with('languages', Language::orderBy('name')->lists('name', 'id'));
 	}
 
