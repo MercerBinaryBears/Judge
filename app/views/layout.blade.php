e6b232b96f42670baf03d39982c83a8ddaa25e9e
Chip Bell <chipbell4@gmail.com>
2014-08-16 22:28:46 -0400

    Moving to bootstrap 3 in the layout file

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,11 +2,8 @@
 <html>
 	<head>
 		<title>Judge</title>
-		@section('scripts')
-			<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
-		@show
 		@section('styles')
-			<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 		@show
 	</head>
 	<body>
@@ -31,5 +28,9 @@
 				@show
 			</div>
 		</div>
+		@section('scripts')
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		@show
 	</body>
 </html>
