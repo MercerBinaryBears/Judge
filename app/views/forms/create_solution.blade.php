5e68240d601557b72d171006fd3533e26a0b0c73
Chip Bell <chipbell4@gmail.com>
2014-08-18 22:15:11 -0400

    Cleaning up solution form

--- app/views/forms/create_solution.blade.php
+++ app/views/forms/create_solution.blade.php
@@ -1,10 +1,11 @@
 {{ Form::open(array('route'=>'store_solution', 'files'=>true)) }}
-{{ Form::label('problem_id', 'Submit a solution to') }}
-{{ Form::select('problem_id', $problems->lists('name','id'))}}
-<br/>
-{{ Form::label('language_id', 'Language') }}
-{{ Form::select('language_id', $languages->lists('name','id')) }}
-<br/>
-{{ Form::file('solution_code') }}
-{{ Form::submit('Submit')}}
+    {{ Form::label('problem_id', 'Submit a solution to') }}
+    {{ Form::select('problem_id', $problems->lists('name','id'), '', array('class' => 'form-control'))}}
+    <br/>
+    {{ Form::label('language_id', 'Language') }}
+    {{ Form::select('language_id', $languages->lists('name','id'), '', array('class' => 'form-control')) }}
+    <br/>
+    {{ Form::file('solution_code', array('class' => 'form-control')) }}
+    <br/>
+    {{ Form::submit('Submit', array('class' => 'btn btn-info'))}}
 {{ Form::close() }}
