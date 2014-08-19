830c38b6dd645cc5f002e8bc56b6680ef8000c5d
Chip Bell <chipbell4@gmail.com>
2014-08-18 20:25:58 -0400

    Cleaning up nav for login form:

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,5 +1,4 @@
-<div class='navbar'>
-	<div class='navbar-inner'>
+<div class='navbar navbar-default' role="navigation">
 	@if(Auth::check())
 		<a class='brand'>{{ Auth::user()->username }}</a>
 		<ul class='nav'>
@@ -16,19 +15,24 @@
 			<li>{{ link_to_route('logout', 'Logout') }}</li>
 		</ul>
 	@else
-		{{ Form::open(array('url'=>'/login', 'class'=>'navbar-form pull-left')) }}
-			<span class='input-prepend'>
-				<span class='add-on'><i class='icon-user'></i></span>
-				{{ Form::text('username', Input::old('username'), array('placeholder'=>'Username')) }}
-			</span>
-			<span class='input-prepend'>
-				<span class='add-on'><i class='icon-lock'></i></span>
-				{{ Form::password('password', array('placeholder'=>'Password')) }}
-			</span>
-			<span class='input-prepend'>
-				{{ Form::submit('Login', array('class'=>'btn')) }}
-			</span>
+		{{ Form::open(array('url'=>'/login', 'class'=>'navbar-form navbar-left')) }}
+            <div class="form-group">
+                <div class='input-group'>
+                    <span class='input-group-addon'>
+                        <span class='glyphicon glyphicon-user'></span>
+                    </span>
+                    {{ Form::text('username', Input::old('username'), array('placeholder'=>'Username', 'class' => 'form-control')) }}
+                </div>
+                <div class='input-group'>
+                    <span class='input-group-addon'>
+                        <span class='glyphicon glyphicon-lock'></span>
+                    </span>
+                    {{ Form::password('password', array('placeholder'=>'Password', 'class' => 'form-control')) }}
+                </div>
+                <span class='input-group'>
+                    {{ Form::submit('Login', array('class'=>'btn btn-info')) }}
+                </span>
+            </div>
 		{{ Form::close() }}
 	@endif
-	</div>
 </div>
