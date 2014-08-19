c0f798a854eecb0142fbb4186d2555fa011aad1f
Chip Bell <chipbell4@gmail.com>
2014-08-18 22:23:24 -0400

    Cleaning up listing form

--- app/views/solutions_judge.blade.php
+++ app/views/solutions_judge.blade.php
@@ -6,20 +6,17 @@
 	</h4>
 	<div class="col-md-6">
 		<h3>Unjudged Solutions</h3>
-		<ul class="solutions unstyled">
 		@foreach($unjudged_solutions as $solution)
-			<li>
-				<dl class='dl-horizontal'>
-					<dt>Problem</dt>
-					<dd>{{ $solution->problem->name }}</dt>
-					<dt>Submitted</dt>
-					<dd>{{ $solution->submissionPrettyDiff() }}</dd>
-					<dt>Team</dt>
-					<dd>{{$solution->user->username}}</dd>
-					<dt></dt>
-					<dd>{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array()) }}</dd>
-				</dl>
-			</li>
+            <dl class='dl-horizontal'>
+                <dt>Problem</dt>
+                <dd>{{ $solution->problem->name }}</dt>
+                <dt>Submitted</dt>
+                <dd>{{ $solution->submissionPrettyDiff() }}</dd>
+                <dt>Team</dt>
+                <dd>{{$solution->user->username}}</dd>
+                <dt></dt>
+                <dd>{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array()) }}</dd>
+            </dl>
 		@endforeach
 		</ul>
 	</div>
