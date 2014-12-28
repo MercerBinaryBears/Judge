5061816e0df6c8ecf368b8336e09a0290095b1f6
Chip Bell <chipbell4@gmail.com>
2014-12-28 00:56:56 -0500

    Listing out judged solutions correctly

--- app/views/solutions_judge.blade.php
+++ app/views/solutions_judge.blade.php
@@ -22,23 +22,20 @@
 	</div>
 	<div class="col-md-6">
 		<h3>Your Judged Solutions</h3>
-		<ul class="solutions unstyled">
 		@foreach($claimed_solutions as $solution)
-			<li>
-				<dl class='dl-horizontal'>
-					<dt>Problem</dt>
-					<dd>{{ $solution->problem->name }}</dt>
-					<dt>Submitted</dt>
-					<dd>{{ $solution->created_at }}</dd>
-					<dt>Team</dt>
-					<dd>{{ $solution->user->username}}</dd>
-					<dt>Judged as</dt>
-					<dd>{{ $solution->solution_state->name }}</dd>
-					<dt></dt>
-					<dd>{{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array()) }}</dd>
-				</dl>
-			</li>
+            <dl class='dl-horizontal'>
+                <dt>Problem</dt>
+                <dd>{{ $solution->problem->name }}</dt>
+                <dt>Submitted</dt>
+                <dd>{{ $solution->created_at }}</dd>
+                <dt>Team</dt>
+                <dd>{{ $solution->user->username}}</dd>
+                <dt>Judged as</dt>
+                <dd>{{ $solution->solution_state->name }}</dd>
+                <dt></dt>
+                <dd>{{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
+            </dl>
+            <hr/>
 		@endforeach
-		</ul>
 	</div>
 @stop
