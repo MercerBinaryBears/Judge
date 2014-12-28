88ff213ecb14e8869064f542178615edbf85f78b
Chip Bell <chipbell4@gmail.com>
2014-12-28 10:43:50 -0500

    Appending version number onto software. Fixes #49

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -21,6 +21,13 @@
 				@section('content')
 				@show
 			</div>
+            @if(file_exists(base_path('VERSION')))
+            <div class="row">
+                <div class="col-md-12 text-center text-muted">
+                    <a href="https://github.com/chipbell4/Judge/tree/{{ file_get_contents(base_path('VERSION')) }}">Version {{ file_get_contents(base_path('VERSION')) }}</a>
+                </div>
+            </div>
+            @endif
 		</div>
 		@section('scripts')
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
