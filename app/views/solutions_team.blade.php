502e13a320cc386d15bf691565f902b1cb828e37
Chip Bell <chipbell4@gmail.com>
2014-12-28 01:10:18 -0500

    Laying out table a little better

--- app/views/solutions_team.blade.php
+++ app/views/solutions_team.blade.php
@@ -2,32 +2,35 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-4">
         @include('forms.create_solution')
+        <hr/>
     </div>
-</div>
-<hr/>
-<div class="row">
-    @foreach($solutions as $solution)
-        <div class="col-md-4">
-            <dl class='dl-horizontal'>
-                <dt>Problem</dt>
-                <dd>{{ $solution->problem->name }}</dt>
-                <dt>Submitted</dt>
-                <dd>{{ $solution->submissionPrettyDiff() }}</dd>
-                <dt>State</dt>
-                <dd>{{ $solution->solutionState->name}}</dd>
-                <dt>Judged By</dt>
-                <dd>
-                    @if($solution->claiming_judge_id != null)
-                        {{$solution->claimingJudge->username}}
-                    @else
-                        No one yet
-                    @endif
-                </dd>
-            </dl>
-        </div>
-    @endforeach
+    <div class="col-md-8">
+        @foreach($solutions as $solution)
+            <div class="row">
+                <div class="col-md-12">
+                    <dl class='dl-horizontal'>
+                        <dt>Problem</dt>
+                        <dd>{{ $solution->problem->name }}</dt>
+                        <dt>Submitted</dt>
+                        <dd>{{ $solution->submissionPrettyDiff() }}</dd>
+                        <dt>State</dt>
+                        <dd>{{ $solution->solutionState->name}}</dd>
+                        <dt>Judged By</dt>
+                        <dd>
+                            @if($solution->claiming_judge_id != null)
+                                {{$solution->claimingJudge->username}}
+                            @else
+                                No one yet
+                            @endif
+                        </dd>
+                    </dl>
+                </div>
+            </div>
+            <hr/>
+        @endforeach
+    </div>
 </div>
 
 @stop
