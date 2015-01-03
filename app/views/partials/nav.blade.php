eae294ae25a9a4e6b8cd8494504798b3b68c776a
Chip Bell <chipbell4@gmail.com>
2015-01-03 00:05:19 -0500

    Making nav items more explicit

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -11,10 +11,10 @@
 				<li> {{ link_to_route('admin_dashboard', 'Admin', array(), array()) }} </li>
 			@endif
 			@if(Auth::user()->judge || Auth::user()->admin)
-				<li> {{ link_to_route('judge_index', 'Judge', array(), array())}} </li>
+				<li> {{ link_to_route('judge_index', 'Judge Problems', array(), array())}} </li>
 			@endif
 			@if(Auth::user()->team || Auth::user()->admin)
-				<li> {{ link_to_route('team_index', 'Team', array(), array()) }} </li>
+				<li> {{ link_to_route('team_index', 'Submit Problems', array(), array()) }} </li>
 			@endif
 			<li>{{ link_to_route('logout', 'Logout') }}</li>
 		</ul>
