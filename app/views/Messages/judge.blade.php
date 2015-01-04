61598b0570bc1935e2ebf470e77f6803ff17d184
Chip Bell <chipbell4@gmail.com>
2015-01-03 23:56:41 -0500

    Basic listing of submitted messages

--- app/views/Messages/judge.blade.php
+++ app/views/Messages/judge.blade.php
@@ -5,7 +5,12 @@
     <div class="col-md-4">
         <h3>Post Global Message</h3>
         @include('forms.create_message')
-
+    </div>
+    <div class="col-md-4">
+        <h3>Unresponded Messages</h3>
+        @foreach($unresponded_messages as $message)
+            @include('Messages.single_message')
+        @endforeach
     </div>
 </div>
 @stop
