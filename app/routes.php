93a8ff3d58797c76d8280385b54bcc83fc99ec5f
Chip Bell <chipbell4@gmail.com>
2015-01-03 19:18:02 -0500

    Coalescing Solutions into a single controller

--- app/routes.php
+++ app/routes.php
@@ -12,26 +12,28 @@ Route::group(array('namespace' => 'Judge\Controllers'), function () {
     // home route. This is where the scoreboard lives.
     Route::get('/', array('as'=>'index', 'uses'=>'HomeController@index'));
 
+    Route::group(array('before' => 'auth'), function () {
+        Route::get('/solutions', array('as' => 'solutions.index', 'uses' => 'SolutionController@index'));
+    });
+
     // judge routes
     Route::group(array('before'=>'judge'), function(){
-        Route::get('/judge',  array('as'=>'judge_index', 'uses'=>'JudgeController@index'));
         // TODO: Perhaps we should embed the user's id into the URL, to be RESTful? However, this
         // means we have more validation to do in that route...
-        Route::get('/judge/solutions/{id}/edit', array('as'=>'edit_solution', 'uses'=>'JudgeController@edit'));
-        Route::post('/judge/solutions/{id}/edit', array('as'=>'update_solution', 'uses'=>'JudgeController@update'));
-        Route::post('/judge/solutions/{id}/unclaim', array('as'=>'unclaim_solution', 'uses'=>'JudgeController@unclaim'));
-        Route::get('/judge/solutions/{id}/package', array('as'=>'solution_package', 'uses'=>'JudgeController@package'));
+        Route::get('/judge/solutions/{id}/edit', array('as'=>'edit_solution', 'uses'=>'SolutionController@edit'));
+        Route::post('/judge/solutions/{id}/edit', array('as'=>'update_solution', 'uses'=>'SolutionController@update'));
+        Route::post('/judge/solutions/{id}/unclaim', array('as'=>'unclaim_solution', 'uses'=>'SolutionController@unclaim'));
+        Route::get('/judge/solutions/{id}/package', array('as'=>'solution_package', 'uses'=>'SolutionController@package'));
 
         Route::get('/judge/messages', array('uses' => 'MessagesController@index'));
     });
 
     // team routes
     Route::group(array('before'=>'team'), function(){
-        Route::get('/team', array('as'=>'team_index', 'uses'=>'TeamController@teamIndex'));
         // there is no create route, since the create form is on the main page for teams
         // TODO: Perhaps we should embed the user's id into the URL, to be RESTful? However, this
         // means we have more validation to do in that route...
-        Route::post('/team/solutions/store', array('as'=>'store_solution', 'uses'=>'TeamController@store'));
+        Route::post('/team/solutions/store', array('as'=>'store_solution', 'uses'=>'SolutionController@store'));
         
         Route::get('/team/messages', array('uses' => 'MessagesController@index'));
     });
