80500b33323358ebaba78d04814a6589079378b6
Chip Bell <chipbell4@gmail.com>
2015-01-07 23:59:17 -0500

    Adding a global message markup

--- app/views/Messages/single_global_message.blade.php
+++ app/views/Messages/single_global_message.blade.php
@@ -1,12 +1,12 @@
-<div>
-@if($message->problem)
-    For {{ $message->problem->name }}
-@else
-    General Message
-@endif
-<br/>
-<pre>{{ $message->text }}</pre>
-<br/>
-From {{ $message->sender->username }}
-
-</div>
+<blockquote>
+    <h4>
+    @if($message->problem)
+        {{ $message->problem->name }}
+    @else
+        General Message
+    @endif
+    </h4>
+    <p>{{ $message->text }}</p>
+    <footer>{{ $message->sender->username }}</footer>
+</blockquote>
+<hr/>
