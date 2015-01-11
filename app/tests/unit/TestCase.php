56cd48b925b20a91c3ff0741700a6309ee918d0b
Chip Bell <chipbell4@gmail.com>
2015-01-11 13:33:47 -0500

    Reorganizing tests

--- app/tests/unit/TestCase.php
+++ app/tests/unit/TestCase.php
@@ -27,6 +27,6 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		$testEnvironment = 'testing';
 
-		return require __DIR__.'/../../bootstrap/start.php';
+		return require __DIR__.'/../../../bootstrap/start.php';
 	}
 }
