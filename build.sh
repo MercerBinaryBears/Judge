d761cafb4f06baec9c72675ada7b89079d6f9697
Chip Bell <chipbell4@gmail.com>
2015-01-11 21:48:22 -0500

    Adding an acceptance test target

--- build.sh
+++ build.sh
@@ -12,6 +12,20 @@ function unit() {
     php vendor/bin/phpunit
 }
 
+function acceptance() {
+    # start the server, and save the pid
+    php artisan serve &
+    sleep 1
+    ps -o pid,command | grep -v grep | grep server.php | cut -d' ' -f1 > .artisan_id.txt
+
+    # run tests
+    vendor/bin/behat
+
+    # kill the server
+    kill `cat .artisan_id.txt`
+    rm .artisan_id.txt
+}
+
 # if a parameter is provided, treat it as the build "targets to run". Useful for local dev
 if [ $1 ]; then
     $1
