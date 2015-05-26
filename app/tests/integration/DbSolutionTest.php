f9a3d41cc99f5b6d4c71ab0d995202671c2b39fb
Chip Bell <chipbell4@gmail.com>
2015-05-26 15:04:38 -0400

    Adding more tests

--- app/tests/integration/DbSolutionTest.php
+++ app/tests/integration/DbSolutionTest.php
@@ -4,10 +4,11 @@ use Carbon\Carbon;
 use Judge\Models\Contest;
 use Judge\Models\Problem;
 use Judge\Models\Solution;
+use Judge\Models\SolutionState;
 
 class DbSolutionTest extends DbTestCase
 {
-    public function testScopeForCurrentContestWithMatch()
+    protected function stubSolution()
     {
         $contest = Contest::create([
             'name' => 'test contest',
@@ -22,7 +23,7 @@ class DbSolutionTest extends DbTestCase
             'judging_output' => 'OUTPUT'
         ]);
 
-        $solution = Solution::create([
+        return Solution::create([
             'problem_id' => $problem->id,
             'user_id' => 1,
             'language_id' => 1,
@@ -30,12 +31,52 @@ class DbSolutionTest extends DbTestCase
             'solution_code' => 'asdf',
             'solution_filename' => 'asdf'
         ]);
+    }
 
+    public function testScopeForCurrentContestWithMatch()
+    {
+        $this->stubSolution();
         $this->assertEquals(1, Solution::forCurrentContest()->count());
     }
 
     public function testScopeForCurrentContestWithNoProblems()
     {
         $this->assertEquals(0, Solution::forCurrentContest()->count());
+    }
+
+    public function testScopeUnjudged()
+    {
+        $solution = $this->stubSolution();
+        $solution->solution_state_id = SolutionState::pending()->id;
+        $solution->save();
+
+        $this->assertEquals(1, Solution::unjudged()->count());
+    }
+
+    public function testScopeUnjudgedWithNonPending()
+    {
+        $solution = $this->stubSolution();
+        $solution->solution_state_id = SolutionState::first()->id;
+        $solution->save();
+
+        $this->assertEquals(0, Solution::unjudged()->count());
+    }
+
+    public function testScopeUnclaimed()
+    {
+        $solution = $this->stubSolution();
+        $solution->claiming_judge_id = null;
+        $solution->save();
+
+        $this->assertEquals(1, Solution::unclaimed()->count());
+    }
+
+    public function testScopeUnclaimedWithClaimed()
+    {
+        $solution = $this->stubSolution();
+        $solution->claiming_judge_id = 1;
+        $solution->save();
+
+        $this->assertEquals(0, Solution::unclaimed()->count());
     }
 }
