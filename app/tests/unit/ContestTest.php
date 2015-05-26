7b7f083e520e69a98f164bbce0f5d8cfee16e9a5
Chip Bell <chipbell4@gmail.com>
2015-05-26 00:37:32 -0400

    Adding tests for the relationships

---
+++ app/tests/unit/ContestTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Judge\Models\Contest;
+
+class ContestTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->contest = new Contest();
+    }
+    
+    public function testProblems()
+    {
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Relations\HasMany', $this->contest->problems());
+    }
+
+    public function testUsers()
+    {
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Relations\BelongsToMany', $this->contest->users());
+    }
+}
