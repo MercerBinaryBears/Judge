baabfaa5f54a28f2d455b424bd373d01799b2865
Chip Bell <chipbell4@gmail.com>
2015-05-28 10:54:21 -0400

    User tests are done

--- app/tests/unit/UserTest.php
+++ app/tests/unit/UserTest.php
@@ -123,4 +123,30 @@ class UserTest extends TestCase
 
         $this->assertEquals(1, $user->problemsSolved());
     }
+
+    public function testGetSetRememberToken()
+    {
+        $user = new User();
+        $user->setRememberToken('ABC');
+        $this->assertEquals('ABC', $user->getRememberToken());
+    }
+
+    public function testGetRememberTokenName()
+    {
+        $user = new User();
+        $this->assertEquals('remember_token', $user->getRememberTokenName());
+    }
+
+    public function testGetReminderEmail()
+    {
+        $user = new User();
+        $user->email = 'EMAIL';
+        $this->assertEquals('EMAIL', $user->getReminderEmail());
+    }
+
+    public function testSentMessages()
+    {
+        $user = new User();
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Relations\HasMany', $user->sentMessages());
+    }
 }
