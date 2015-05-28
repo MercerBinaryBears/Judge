73246dfdc870b10b2d50fe429d86833fffd2c28d
Chip Bell <chipbell4@gmail.com>
2015-05-27 23:01:51 -0400

    Finishing repository tests :)

---
+++ app/tests/integration/DbContestRepositoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Judge\Repositories\ContestRepository;
+use Laracasts\TestDummy\Factory;
+
+class DbContestRepositoryTest extends DbTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->repo = new ContestRepository();
+    }
+
+    public function testTeamsForContestWithNoTeams()
+    {
+        $contest = Factory::create('contest');
+        $user = Factory::create('judge');
+        $contest->users()->attach($user->id);
+        $this->assertCount(0, $this->repo->teamsForContest());
+    }
+
+    public function testTeamsForContestWithTeams()
+    {
+        $contest = Factory::create('contest');
+        $user = Factory::create('team');
+        $contest->users()->attach($user->id);
+        $this->assertCount(1, $this->repo->teamsForContest());
+    }
+}
