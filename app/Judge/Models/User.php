22b8c0d5a7bf10ba6984d9f60763b1b6c23f8252
Chip Bell <chipbell4@gmail.com>
2015-06-01 20:27:39 -0400

    Moving some things around. Should be REALLY broken right now

--- app/Judge/Models/User.php
+++ app/Judge/Models/User.php
@@ -53,55 +53,11 @@ class User extends Base implements UserInterface, RemindableInterface
     }
 
     /**
-     * Scores the user's solution for this problem.
-     * 20 pts added for each incorrect solution, plus 1 pt
-     * for each additional minute since contest start time.
-     *
-     * @param problem $problem the problem to score
-     * @return int the number of points
-     */
-    public function pointsForProblem(Problem $problem)
-    {
-        // get the start time for the contest
-        $starts_at = new Carbon($this->cachedContest()->starts_at);
-
-        // if they didn't solve it, return 0
-        if (! $this->solvedProblem($problem)) {
-            return 0;
-        }
-
-        $incorrect_count = $this->incorrectSubmissionCountForProblem($problem);
-
-        $earliest_solution = $this->earliestCorrectSolutionForProblem($problem);
-
-        $minutes_since_contest = $earliest_solution->created_at->diffInMinutes($starts_at);
-        
-        return $incorrect_count * 20 + $minutes_since_contest;
-    }
-
-    /**
      * Gets all of the solutions submitted by a user.
      */
     public function solutions()
     {
         return $this->hasMany('Judge\Models\Solution');
-    }
-
-    /**
-     * Scores the solutions submitted by a user for a
-     * given contest.
-     *
-     * @param contest $contest the contest to score points on
-     * @return int the total number of points for this user
-     */
-    public function totalPoints($contest = null)
-    {
-        $problems = $this->cachedProblems($contest);
-        $points = 0;
-        foreach ($problems as $problem) {
-            $points += $this->pointsForProblem($problem);
-        }
-        return $points;
     }
 
     /**
@@ -125,112 +81,6 @@ class User extends Base implements UserInterface, RemindableInterface
 
         // trim off the excess
         return substr($s, 0, $length);
-    }
-
-    /**
-     * Provides a summary of the given contest for this user
-     *
-     * @param Contest $contest The contest to summarize
-     * @return array The summary data
-     */
-    public function contestSummary($contest)
-    {
-        $summary = new ContestSummary;
-
-        $summary->user = $this;
-        
-        $summary->problems_solved = $this->problemsSolved($contest);
-
-        $summary->penalty_points = $this->totalPoints($contest);
-
-        $summary->problem_summaries = array();
-
-        foreach ($contest->problems as $problem) {
-            $problem_info = array();
-            $problem_info['points_for_problem'] = $this->pointsForProblem($problem);
-            $problem_info['num_submissions'] = $this->incorrectSubmissionCountForProblem($problem)
-                + $this->solvedProblem($problem);
-
-            $summary->problem_summaries[] = $problem_info;
-        }
-        return $summary;
-    }
-
-    /*
-     * Calculates the number of problems solved for user in a provide
-     * contest. If no contest is provided, defaults to the most recent current
-     * contest.
-     *
-     * @param Contest $contest
-     * @return int
-     */
-    public function problemsSolved(Contest $c = null)
-    {
-        $problems = $this->cachedProblems($c);
-
-        /*
-         * Loop over every problem, checking if there is a solution
-         * that is solved
-         */
-        $total = 0;
-        foreach ($problems as $problem) {
-            if ($this->solvedProblem($problem)) {
-                $total++;
-            }
-        }
-
-        // now sum up the solved states
-        return $total;
-    }
-
-    /**
-     * Returns true if this user solved the given problem
-     */
-    public function solvedProblem(Problem $problem)
-    {
-        return App::make('Judge\Repositories\SolutionRepository')->hasCorrectSolutionFromUser($this, $problem);
-    }
-
-    /**
-     * Calculates the number of incorrect submissions for a given problem
-     */
-    public function incorrectSubmissionCountForProblem(Problem $p)
-    {
-        return App::make('Judge\Repositories\SolutionRepository')
-            ->incorrectSubmissionCountFromUserFromProblem($this, $p);
-    }
-
-    public function earliestCorrectSolutionForProblem(Problem $problem)
-    {
-        return App::make('Judge\Repositories\SolutionRepository')
-            ->earliestCorrectSolutionFromUserForProblem($this, $problem);
-    }
-
-    public function cachedContest()
-    {
-        if (!isset($this->cached_contest)) {
-            $this->cached_contest = App::make('Judge\Repositories\ContestRepository')->firstCurrent();
-        }
-
-        return $this->cached_contest;
-    }
-
-    public function cachedProblems(Contest $c = null)
-    {
-        if (!isset($this->cached_problems)) {
-            $this->cached_problems = App::make('Judge\Repositories\ContestRepository')->problemsForContest($c);
-        }
-
-        return $this->cached_problems;
-    }
-
-    public function cachedSolutions(Contest $c = null)
-    {
-        if (!isset($this->cached_solutions)) {
-            $this->cached_solutions = App::make('Judge\Repositories\SolutionRepository')->forUserInContest($this, $c);
-        }
-
-        return $this->cached_solutions;
     }
 
     public function getRememberToken()
---
+++ app/tests/unit/ContestSummaryFactoryTest.php
@@ -0,0 +1,100 @@
+<?php
+
+class ContestSummaryFactoryTest extends DbTestCase
+{
+    public function testPointsForProblemIsZeroIfNoSolutionYet()
+    {
+        $contest = Mockery::mock();
+        $contest->starts_at = '2015-01-01 00:00:00';
+        $user = Mockery::mock('Judge\Models\User[cachedContest,solvedProblem]');
+        $user->shouldReceive('cachedContest')->once()->andReturn($contest);
+        $user->shouldReceive('solvedProblem')->once()->andReturn(false);
+
+        $problem = Mockery::mock('Judge\Models\Problem');
+
+        $this->assertEquals(0, $user->pointsForProblem($problem));
+    }
+
+    public function testPointsForProblemCalculatedCorrectly()
+    {
+        $contest = Mockery::mock();
+        $contest->starts_at = '2015-01-01 00:00:00';
+        $user = Mockery::mock('Judge\Models\User[cachedContest,solvedProblem,incorrectSubmissionCountForProblem,earliestCorrectSolutionForProblem]');
+        $user->shouldReceive('cachedContest')->once()->andReturn($contest);
+        $user->shouldReceive('solvedProblem')->once()->andReturn(true);
+
+        // two incorrect submissions
+        $user->shouldReceive('incorrectSubmissionCountForProblem')->once()->andReturn(2);
+
+        // 50 minutes after contest start
+        $solution = Mockery::mock();
+        $solution->created_at = Carbon::create(2015, 1, 1, 0, 50, 0);
+        $user->shouldReceive('earliestCorrectSolutionForProblem')->once()->andReturn($solution);
+        
+        $problem = Mockery::mock('Judge\Models\Problem');
+
+        $this->assertEquals(20 + 20 + 50, $user->pointsForProblem($problem));
+    }
+
+    public function testSolvedProblem()
+    {
+        $repo = Mockery::mock()->shouldReceive('hasCorrectSolutionFromUser')->andReturn('TEST')->getMock();
+        App::shouldReceive('make')->once()->andReturn($repo);
+
+        $user = new User();
+        $this->assertEquals('TEST', $user->solvedProblem(Mockery::mock('Judge\Models\Problem')));
+    }
+
+    public function testContestSummary()
+    {
+        $user = Mockery::mock('Judge\Models\User[problemsSolved,totalPoints,pointsForProblem,incorrectSubmissionCountForProblem,solvedProblem]');
+
+        $user->shouldReceive('problemsSolved')->once()->andReturn(1);
+        $user->shouldReceive('totalPoints')->once()->andReturn(100);
+        $user->shouldReceive('pointsForProblem')->once()->andReturn(10);
+        $user->shouldReceive('incorrectSubmissionCountForProblem')->once()->andReturn(1);
+        $user->shouldReceive('solvedProblem')->once()->andReturn(1);
+
+        $contest = Mockery::mock('Judge\Models\Contest');
+        $contest->shouldReceive('getAttribute')->once()->with('problems')->andReturn([Mockery::mock('Judge\Models\Problem')]);
+
+        $result = $user->contestSummary($contest);
+
+        $this->assertEquals(1, $result->problems_solved);
+        $this->assertEquals(100, $result->penalty_points);
+        $this->assertEquals(10, $result->problem_summaries[0]['points_for_problem']);
+        $this->assertEquals(2, $result->problem_summaries[0]['num_submissions']);
+    }
+
+    public function testProblemsSolvedWithUnsolvedProblems()
+    {
+        $user = Mockery::mock('Judge\Models\User[cachedProblems,solvedProblem]');
+        $user->shouldReceive('cachedProblems')->once()->andReturn([
+            Mockery::mock('Judge\Models\Problem')
+        ]);
+        $user->shouldReceive('solvedProblem')->once()->andReturn(false);
+
+        $this->assertEquals(0, $user->problemsSolved());
+    }
+
+    public function testProblemsSolvedWithSolvedProblems()
+    {
+        $user = Mockery::mock('Judge\Models\User[cachedProblems,solvedProblem]');
+        $user->shouldReceive('cachedProblems')->once()->andReturn([
+            Mockery::mock('Judge\Models\Problem')
+        ]);
+        $user->shouldReceive('solvedProblem')->once()->andReturn(true);
+
+        $this->assertEquals(1, $user->problemsSolved());
+    }
+
+    public function testTotalPoints()
+    {
+        $user = Mockery::mock('Judge\Models\User[cachedProblems,pointsForProblem]');
+        $problem = Mockery::mock('Judge\Models\Problem');
+        $user->shouldReceive('cachedProblems')->once()->andReturn([$problem]);
+        $user->shouldReceive('pointsForProblem')->once()->andReturn(123);
+
+        $this->assertEquals(123, $user->totalPoints());
+    }
+}
