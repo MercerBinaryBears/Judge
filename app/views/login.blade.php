c28825257587b3593b211845a2457e47e3a6244f
Chip Bell <chipbell4@gmail.com>
2015-06-22 23:19:55 -0400

    Adding a logo

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,6 +4,9 @@
 <form action="/login" role="form" class="form login-form" method="post">
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
+            <div class="text-center">
+                <img src="/logo.png" style="height:5em;" />
+            </div>
             {{ Form::text('username', Input::old('username'), array('placeholder' => 'Username', 'class' => 'form-control')) }}
             {{ Form::password('password', array('placeholder'=>'Password', 'class' => 'form-control')) }}
             {{ Form::submit('Login', array('class'=>'btn btn-info')) }}
