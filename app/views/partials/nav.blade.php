5c49088bb55bf9ec759f624bfbd49dc49709436d
Chip Bell <chipbell4@gmail.com>
2015-06-22 22:50:04 -0400

    Swapping navigation around

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,11 +1,10 @@
-<div class='navbar navbar-default navbar-fixed-top' role="navigation">
-    <a class="navbar-brand nav-logo-a" style="padding:5px 5px;">
-		<img src='/logo.png' alt='Judge Logo' class="logo nav-logo" style="height: 40px;"/>
+<div class="sidebar">
+    <a class="admin-link" href="/">
+        <img src='/logo.png' alt='Judge Logo' class="logo nav-logo" style="height: 40px;"/>
     </a>
-    <a class="navbar-brand"> {{ $contest_name }} </a>
-	@if(Auth::check())
+    @if(Auth::check())
         @include('partials.nav_links')
-	@else
+    @else
         @include('forms.login')
-	@endif
+    @endif
 </div>
--- app/views/partials/nav_links.blade.php
+++ app/views/partials/nav_links.blade.php
@@ -1,14 +1,16 @@
-<ul class='nav navbar-nav'>
-    <li><a>{{ Auth::user()->username }}</a></li>
-    @if(Auth::user()->admin)
-        <li> <a href="/admin"><i class="fa fa-database"></i></a></li>
-    @endif
-    @if(Auth::user()->judge || Auth::user()->admin)
-        <li> <a href="/solutions"><i class="fa fa-gavel"></i></a></li>
-    @endif
-    @if(Auth::user()->team)
-        <li> <a href="/solutions"><i class="fa fa-code"></i></a></li>
-    @endif
-    <li><a href="/messages"><i class="fa fa-envelope-o"></i></a></li>
-    <li><a href="/logout"><i class="fa fa-sign-out"></i></a></li>
-</ul>
+<span class="admin-link">{{ Auth::user()->username }}</span>
+
+@if(Auth::user()->admin)
+ <a class="admin-link" href="/admin"><i class="fa fa-database"></i></a>
+@endif
+
+@if(Auth::user()->judge || Auth::user()->admin)
+<a class="admin-link" href="/solutions"><i class="fa fa-gavel"></i></a>
+@endif
+
+@if(Auth::user()->team)
+<a class="admin-link" href="/solutions"><i class="fa fa-code"></i></a>
+@endif
+
+<a class="admin-link" href="/messages"><i class="fa fa-envelope-o"></i></a>
+<a class="admin-link" href="/logout"><i class="fa fa-sign-out"></i></a>
