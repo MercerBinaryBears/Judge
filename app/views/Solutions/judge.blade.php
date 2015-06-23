5086b706e5d59aff333e39463dafc0a0da62e926
Chip Bell <chipbell4@gmail.com>
2015-06-22 23:33:59 -0400

    Fixing a float issue

--- app/views/Solutions/judge.blade.php
+++ app/views/Solutions/judge.blade.php
@@ -4,38 +4,40 @@
 	<h4 class="well well-small">
 		Your API key is {{ $api_key }}
 	</h4>
-	<div class="col-md-6">
-		<h3>Unjudged Solutions</h3>
-		@foreach($unjudged_solutions as $solution)
-            <dl class='dl-horizontal'>
-                <dt>Problem</dt>
-                <dd>{{ $solution->problem->name }}</dt>
-                <dt>Submitted</dt>
-                <dd>{{ $solution->submissionPrettyDiff() }}</dd>
-                <dt>Team</dt>
-                <dd>{{$solution->user->username}}</dd>
-                <dt></dt>
-                <dd>{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
-            </dl>
-		@endforeach
-		</ul>
-	</div>
-	<div class="col-md-6">
-		<h3>Your Judged Solutions</h3>
-		@foreach($claimed_solutions as $solution)
-            <dl class='dl-horizontal bg-{{ $solution->solution_state->bootstrap_color }}'>
-                <dt>Problem</dt>
-                <dd>{{ $solution->problem->name }}</dt>
-                <dt>Submitted</dt>
-                <dd>{{ $solution->submissionPrettyDiff() }}</dd>
-                <dt>Team</dt>
-                <dd>{{ $solution->user->username}}</dd>
-                <dt>Judged as</dt>
-                <dd>{{ $solution->solution_state->name }}</dd>
-                <dt></dt>
-                <dd>{{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
-            </dl>
-            <hr/>
-		@endforeach
-	</div>
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Unjudged Solutions</h3>
+            @foreach($unjudged_solutions as $solution)
+                <dl class='dl-horizontal'>
+                    <dt>Problem</dt>
+                    <dd>{{ $solution->problem->name }}</dt>
+                    <dt>Submitted</dt>
+                    <dd>{{ $solution->submissionPrettyDiff() }}</dd>
+                    <dt>Team</dt>
+                    <dd>{{$solution->user->username}}</dd>
+                    <dt></dt>
+                    <dd>{{ link_to_route('edit_solution', 'Claim', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
+                </dl>
+            @endforeach
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <h3>Your Judged Solutions</h3>
+            @foreach($claimed_solutions as $solution)
+                <dl class='dl-horizontal bg-{{ $solution->solution_state->bootstrap_color }}'>
+                    <dt>Problem</dt>
+                    <dd>{{ $solution->problem->name }}</dt>
+                    <dt>Submitted</dt>
+                    <dd>{{ $solution->submissionPrettyDiff() }}</dd>
+                    <dt>Team</dt>
+                    <dd>{{ $solution->user->username}}</dd>
+                    <dt>Judged as</dt>
+                    <dd>{{ $solution->solution_state->name }}</dd>
+                    <dt></dt>
+                    <dd>{{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
+                </dl>
+                <hr/>
+            @endforeach
+        </div>
+    </div>
 @stop
