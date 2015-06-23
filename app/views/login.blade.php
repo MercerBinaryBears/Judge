3bf59ba58bd9254a50623ef45f867421567240f1
Chip Bell <chipbell4@gmail.com>
2015-06-22 23:30:36 -0400

    Styling up the login form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,7 +4,7 @@
 <form action="/login" role="form" class="form login-form" method="post">
     <div class="row">
         <div class="col-md-4 col-md-offset-4 text-center">
-            <img src="/logo.png" style="height:5em;" />
+            <img src="/logo.png" />
             {{ Form::text('username', Input::old('username'), array('placeholder' => 'Username', 'class' => 'form-control')) }}
             {{ Form::password('password', array('placeholder'=>'Password', 'class' => 'form-control')) }}
             {{ Form::submit('Login', array('class'=>'btn btn-default')) }}
