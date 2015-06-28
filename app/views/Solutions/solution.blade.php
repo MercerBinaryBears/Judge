ced164023ecd91cfa41241cd57f72359ac9614b3
Chip Bell <chipbell4@gmail.com>
2015-06-28 18:58:33 -0400

    First approximation for using a panel

--- app/views/Solutions/solution.blade.php
+++ app/views/Solutions/solution.blade.php
@@ -1,27 +1,30 @@
-<dl class='dl-horizontal bg-{{ $solution->solution_state->bootstrap_color }}'>
-    <dt>Problem</dt>
-    <dd>{{ $solution->problem->name }}</dt>
-    <dt>Submitted</dt>
-    <dd>{{ $solution->submissionPrettyDiff() }}</dd>
-    @if(Auth::user()->team)
-        <dt>Judged By</dt>
-        <dd>
-            @if($solution->claiming_judge_id != null)
-                {{$solution->claimingJudge->username}}
-            @else
-                No one yet
-            @endif
-        </dd>
-    @else
-        <dt>Team</dt>
-        <dd>{{ $solution->user->username}}</dd>
-    @endif
-    <dt>Judged as</dt>
-    <dd>{{ $solution->solution_state->name }}</dd>
-    <dt></dt>
+<div class="panel panel-{{ $solution->solution_state->bootstrap_color }}">
+    <div class="panel-heading">
+        <div class="panel-title">
+            {{ $solution->problem->name }}
+        </div>
+        {{ $solution->submissionPrettyDiff() }}
+    </div>
+    <div class="panel-body">
+        @if(Auth::user()->team)
+            <dt>Judged By</dt>
+            <dd>
+                @if($solution->claiming_judge_id != null)
+                    {{$solution->claimingJudge->username}}
+                @else
+                    No one yet
+                @endif
+            </dd>
+        @else
+            <dt>Team</dt>
+            <dd>{{ $solution->user->username}}</dd>
+        @endif
+        <dt>Judged as</dt>
+        <dd>{{ $solution->solution_state->name }}</dd>
+    </div>
     @if(Auth::user()->judge || Auth::user()->admin)
-    <dd>{{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
+    <div class="panel-footer">
+        {{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}
+    </div>
     @endif
-    </dl>
-<hr/>
-
+</div>
