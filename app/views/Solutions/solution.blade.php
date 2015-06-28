ab4ba30533bffb97b228d4e6bf927db03829caed
Chip Bell <chipbell4@gmail.com>
2015-06-28 18:54:06 -0400

    Using partial for teams too

--- app/views/Solutions/solution.blade.php
+++ app/views/Solutions/solution.blade.php
@@ -3,12 +3,25 @@
     <dd>{{ $solution->problem->name }}</dt>
     <dt>Submitted</dt>
     <dd>{{ $solution->submissionPrettyDiff() }}</dd>
-    <dt>Team</dt>
-    <dd>{{ $solution->user->username}}</dd>
+    @if(Auth::user()->team)
+        <dt>Judged By</dt>
+        <dd>
+            @if($solution->claiming_judge_id != null)
+                {{$solution->claimingJudge->username}}
+            @else
+                No one yet
+            @endif
+        </dd>
+    @else
+        <dt>Team</dt>
+        <dd>{{ $solution->user->username}}</dd>
+    @endif
     <dt>Judged as</dt>
     <dd>{{ $solution->solution_state->name }}</dd>
     <dt></dt>
+    @if(Auth::user()->judge || Auth::user()->admin)
     <dd>{{ link_to_route('edit_solution', 'Edit', array('id'=>$solution->id), array('class' => 'btn btn-default')) }}</dd>
+    @endif
     </dl>
 <hr/>
 
