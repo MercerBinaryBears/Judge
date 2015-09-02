1ea49167955dd1b765b417af8585a106f8f824c2
Chip Bell <chipbell4@gmail.com>
2015-09-02 19:34:40 -0400

    Not using dev packages in deployment

--- pack.sh
+++ pack.sh
@@ -8,6 +8,12 @@ for folder in judging_input judging_output logs sessions solution_code views; do
     rm -f app/storage/$folder/*
 done
 
+# Re install composer to only include non-dev packages
+composer install --no-dev
+
 # now tar everything up
 files="app/commands app/config app/database app/Judge app/lang app/library app/start app/storage app/views bootstrap public vendor artisan"
 tar czvf site.tgz $files
+
+# Go back to development mode
+composer install
