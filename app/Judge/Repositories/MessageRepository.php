3ac0a59219857d18464970b4f2e55f3530127c9e
Chip Bell <chipbell4@gmail.com>
2015-09-06 11:11:19 -0400

    Using a default contest if none is provided. But we're not doing anything with it...

--- app/Judge/Repositories/MessageRepository.php
+++ app/Judge/Repositories/MessageRepository.php
@@ -1,19 +1,37 @@
 <?php namespace Judge\Repositories;
 
+use Judge\Repositories\ContestRepository;
 use Judge\Models\User;
 use Judge\Models\Message;
 
 class MessageRepository
 {
-    public function allGlobal()
+    public function __construct(ContestRepository $contests)
     {
+        $this->contests = $contests;
+    }
+
+    protected function resolveContest(Contest $contest = null) {
+        if($contest) {
+            return $contest;
+        }
+
+        return $this->contests->firstCurrent();
+    }
+
+    public function allGlobal(Contest $contest = null)
+    {
+        $contest = $this->resolveContest($contest);
+
         return Message::whereIsGlobal(true)
             ->orderBy('created_at', 'DESC')
             ->get();
     }
 
-    public function unresponded()
+    public function unresponded(Contest $contest = null)
     {
+        $contest = $this->resolveContest($contest);
+
         return Message::whereIsGlobal(false)
             ->whereResponderId(null)
             ->orderBy('created_at', 'ASC')
