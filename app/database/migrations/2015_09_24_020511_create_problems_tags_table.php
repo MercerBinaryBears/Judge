f7b29baa46437971698243c29a2c1a401b016717
Chip Bell <chipbell4@gmail.com>
2015-09-23 22:09:23 -0400

    Creating the tables

---
+++ app/database/migrations/2015_09_24_020511_create_problems_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProblemsTagsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('problems_tags', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('problem_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+            $table->foreign('problem_id')->references('id')->on('problems')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('problems_tags');
+	}
+
+}
