84f0a852594e1dce28be4bd1415fd0fea221dd27
Chip Bell <cbell@pbs.org>
2017-02-25 19:27:06 -0500

    Replacing the old macro and trying out the new flash lib

--- app/library/macros.php
+++ app/library/macros.php
@@ -7,23 +7,3 @@
 | This file contains custom HTML macros to help with form building, etc.
 |
 */
-
-/**
- * Flashes any error messages to the page if there are stored in Session
- */
-HTML::macro('flash', function() {
-	$html = "<div class\'error\'>";
-	if(Session::has('error')) {
-		$error = Session::get('error');
-		if(is_string($error)) {
-			$html .= $error;
-		}
-		else {
-			$html .= "<ul>";
-			$html .= implode('', $error->all('<li>:message</li>'));
-			$html .= "</ul>";
-		}
-	}
-	$html .= "</div>";
-	return $html;
-});
